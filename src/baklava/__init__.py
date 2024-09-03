4bb6efcab8ecf9ff373455cae88fb8d5adfd1068
Koen Nijbroek <koennijbroek@gmail.com>
2024-09-03 15:03:11 +0200

    first commit

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Koen Nijbroek"
+__email__ = "koennijbroek@gmail.com"
+__version__ = "0.1.0"
